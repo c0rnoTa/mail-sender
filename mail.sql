f941af662166141f3f7662d07743010bb3d152a0
Anton Zakharov <c0rnota@gmail.com>
2021-09-30 09:19:34 +0300

    Update table DDL

--- mail.sql
+++ mail.sql
@@ -4,5 +4,7 @@ create table stats
     receiver    varchar(255) not null
         primary key,
     send_status int          null,
-    send_time   datetime     null
+    send_time   datetime     null,
+    receive_status int          null,
+    receive_time   datetime     null
 );
